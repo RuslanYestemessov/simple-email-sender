6778e0ba831a243e3971447d889c5cedd02e0805
Yestemessov <ruslan.yestemessov@gmail.com>
2023-03-26 22:20:22 +0600

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*.yestemessov.kz',
+      credentials: true,
+    },
+  });
   await app.listen(8465);
 }
 bootstrap();
